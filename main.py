7dcc0b055a35f5148f4e1ee2ebb4ac07963a0160
Teplooo <mmalysev914@gmail.com>
2025-05-16 17:45:33 +0400

    Часть программы с txt файлами

--- main.py
+++ main.py
@@ -1,18 +1,70 @@
-#https://git.altlinux.org/beehive/logs/Sisyphus/x86_64/latest/error/aces_container-1.0.2-alt1_8
 import requests
+import re
+import os
+
+
+def parse_errors(log_file):
+    errors = []
+    with open(log_file, 'r',
+              #encoding='utf-8'
+              ) as f:
+        log_content = f.read()
+
+    # Регулярное выражение для ошибок сборки
+    build_error_pattern = re.compile(
+        r'(error|ERROR|Error|failed|FAILED|Could NOT find|could not find):?\s*(.*?)(?=\n\S|\Z)',
+        re.DOTALL | re.IGNORECASE
+    )
+
+    # Поиск ошибок сборки
+    for match in build_error_pattern.finditer(log_content):
+        if not any(match.group(0) in e['description'] for e in errors):
+            errors.append({
+                'description': match.group(0).strip()
+            })
+
+    return errors
+
+
+def analyze_logs(logs_dir):
+    results = {}
+    for filename in os.listdir(logs_dir):
+        if filename.endswith('.txt'):
+            filepath = os.path.join(logs_dir, filename)
+            errors = parse_errors(filepath)
+            if errors:
+                results[filename] = errors
+
+    return results
 
 # Замените на нужный эндпоинт или параметры
-url = "https://git.altlinux.org/beehive/logs/Sisyphus/x86_64/latest/error/aces_container-1.0.2-alt1_8"
-
+url = input("Введите url")
 response = requests.get(url)
 response.raise_for_status()  # Проверка на ошибки
+responses = response.text.split() #Вся страница, разбитая построчно
+herfs = []
 
-# Если API возвращает JSON
-try:
-    data = response.json()
-    print("Полученные данные (JSON):")
-    print(data)
-except ValueError:
-    # Если возвращается просто текст
-    print("Полученный текст:")
-    print(response.text)
+#Достаем части ссылок на логи
+for tag in responses:
+    if tag[:6] == 'href="':
+        herfs.append(tag.split(sep='"')[1])
+for herf in herfs[2:]:
+    file = open(f"logs/{herf}.txt", "w")
+    try:file.write(requests.get(f"{url}{herf}").text)
+    except:pass
+    file.close()
+
+
+
+
+log_directory = "logs"
+analysis_results = analyze_logs(log_directory)
+k = 0
+    # Вывод результатов
+for filename, errors in analysis_results.items():
+    print(f"\nФайл: {filename}")
+    k += 1
+    for error in errors:
+        print(f'''"{error['description']}\n"''')
+
+print(k)
